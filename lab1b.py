b3ae891d6035710b6c97e9e4d336d908a6bce22c
MD YASIN UDDIN AKASH <65915760+myuakash96@users.noreply.github.com>
2021-02-18 09:25:02 -0500

    Add files via upload
    LAB 1

---
+++ lab1b.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Description: This program will output the question "How old are you" with the value of a object called "name" with value called "Isaac" to the screen.'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+
+name = 'Isaac?'
+print('How old are you ' + name)
---
+++ lab1d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is also known as a comment,
+# these lines are ignored by the python interpreter even if
+# they contain code. The very first line is called a Shebang line, 
+# it is used to tell the system which interpreter to 
+# use(python2, python3, bash, etc). 
+
+x = 10
+y = 2
+z = 5
+
+print( '10 + 2 * 5 = 20' ) 
